8c543b6c3bc490f1520032f14b60ce631c36eb4f
AllenYeh <Allen_Yeh@systemweb.com.tw>
2025-10-07 17:24:45 +0800

    🆙update：更改接到 503 狀態碼導向錯誤頁處理

--- src/handler/handleError.js
+++ src/handler/handleError.js
@@ -80,7 +80,7 @@ export default err => {
       err.message = '網路錯誤(502)'
       break
     case 503:
-      err.message = '服務不可用(503)'
+      err.message = Message || '服務不可用(503)'
       break
     case 504:
       err.message = '網路超時(504)'
